37982b94c1fa1387dffbdc9d2c30c722aa966a06
Dana Nurse <dananurse46@gmail.com>
2025-01-30 16:35:46 -0400

    Updated VolunteersPage.jsx with placeholder content

---
+++ frontend/volunteer-portal-project/src/pages/VolunteersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function VolunteersPage() {
+    return (
+    <div>
+        <h1>This page will have a form for volunteers and example opportunities</h1>
+    </div>
+    );
+};
+
+export default VolunteersPage;
