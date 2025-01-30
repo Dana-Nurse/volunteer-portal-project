8362c0345181bb9a4aa0a17c7d1761dd18e879ff
Dana Nurse <dananurse46@gmail.com>
2025-01-30 16:33:28 -0400

    Updated NavBar.jsx

---
+++ frontend/volunteer-portal-project/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+    <nav>
+        <h2>My Project</h2>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Volunteers</a></li>
+            <li><a href="#">Corporate</a></li>
+            <li><a href="#">Request</a></li>
+            <li><a href="#">Benefits</a></li>
+            <li><a href="#">Info</a></li>
+        </ul>
+    </nav>
+    );
+}
+
+export default NavBar;
